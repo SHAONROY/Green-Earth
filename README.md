146aef0c4b4153aedd7323a71695b96aa7d3df15
SHAON_MAJUMDER <saonroy15@gmail.com>
2025-09-09 22:18:07 +0600

    Complete Some Functionlity

---
+++ README.md
@@ -0,0 +1,23 @@
+Assignment Question with Answer:
+
+1. What is the difference between var, let, and const?
+   Ans:
+   var → function-scoped, can be re-declared and updated.
+   let → block-scoped, can be updated but not re-declared in the same scope.
+   const → block-scoped, cannot be updated or re-declared;
+
+2.What is the difference between map(), forEach(), and filter()?
+
+forEach() → Loops through items, doesn’t return anything, just runs a function.
+
+map() → Loops through items, returns a new array with the results of your function.
+
+filter() → Loops through items, returns a new array with only items that pass a condition.
+
+3.What are arrow functions in ES6?
+Arrow functions are a shorter way to write functions in JavaScript (ES6). They use => and are cleaner and simpler than regular functions.
+4.How does destructuring assignment work in ES6?
+Destructuring assignment in ES6 lets you extract values from arrays or objects into separate variables easily.
+5.Explain template literals in ES6. How are they different from string concatenation?
+
+Template literals in ES6 are a way to write strings easily with variables and expressions using backticks ` instead of quotes
--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const displayDefaultCardData = (cards) => {
     );
     defaultDiv.innerHTML = `
     <img class="w-full rounded-md max-h-[400px]" src="${card.image}" alt="" srcset="" />
-  <h4 class="text-[#1F2937] text-2xl font-bold pt-3">${card.name}</h4>
+  <h4 onclick="loadPlantDetail(${card.id})" class="text-[#1F2937] text-2xl font-bold pt-3" >${card.name}</h4>
   <p class="text-[#1F2937] py-3">
     ${card.description}
   </p>
@@ -34,9 +34,11 @@ const displayDefaultCardData = (cards) => {
     </p>
   </div>
   <button
-    class="bg-[#15803D] text-[#fff] text-2xl px-5 py-3 rounded-full font-semibold w-full"
+    class="bg-[#15803D] text-[#fff] text-2xl px-5 py-3 rounded-full font-semibold w-full add-cart"
   >
-    Donate Now
+    
+      Add to Cart
+    
   </button>`;
     treeContainer.append(defaultDiv);
   }
@@ -48,9 +50,10 @@ const categoriesDataFunction = () => {
     .then((res) => res.json())
     .then((json) => DisplayCategories(json.categories));
 };
+// Add Active Button Function
 const removeActiveBtn = () => {
-  const treeBtn = document.querySelectorAll(".tree-btn");
-  treeBtn.forEach((btn) => btn.classList.remove("active"));
+  const treeBtn = document.querySelectorAll(".tree-btn"); //get all button to remove active button
+  treeBtn.forEach((btn) => btn.classList.remove("active")); //Remove All Active Button
 };
 // Fetch Card Data
 const loadTreeData = (id) => {
@@ -58,11 +61,35 @@ const loadTreeData = (id) => {
   fetch(url)
     .then((res) => res.json())
     .then((tree) => {
-      removeActiveBtn();
+      removeActiveBtn(); //Call the Remove Active button Function
       const treeBtn = document.getElementById(`tree-btn-${id}`);
       treeBtn.classList.add("active");
       displayTreeData(tree.plants);
     });
+};
+// Display Your Cart Data Dynamic
+
+// Load The Plants Details
+const loadPlantDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayPlantDetail(details.plants);
+};
+
+const displayPlantDetail = (plant) => {
+  const detailBox = document.getElementById("details-container");
+  detailBox.innerHTML = `
+  <h2 class="text-3xl text-[#000000] font-bold pb-5">${plant.name}</h2>
+          <img class="w-full" src="${plant.image}" alt="" />
+          <p class="text-[#1F2937] text-2xl py-5">Category:${plant.category}</p>
+          <p class="text-[#1F2937] text-2xl py-5">
+            Price:<i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}
+          </p>
+          <p class="text-[#1F2937] text-2xl py-5">Description: ${plant.description}</p>
+  
+  `;
+  document.getElementById("plant_model").showModal();
 };
 // Display Card Data
 const displayTreeData = (trees) => {
@@ -80,7 +107,7 @@ const displayTreeData = (trees) => {
     );
     treeDiv.innerHTML = `
   <img class="w-full rounded-md max-h-[400px]" src="${tree.image}" alt="" srcset="" />
-  <h4 class="text-[#1F2937] text-2xl font-bold pt-3">${tree.name}</h4>
+  <h4 class="text-[#1F2937] text-2xl font-bold pt-3" onclick="loadPlantDetail(${tree.id})">${tree.name}</h4>
   <p class="text-[#1F2937] py-3">
     ${tree.description}
   </p>
@@ -94,13 +121,14 @@ const displayTreeData = (trees) => {
       <i class="fa-solid fa-bangladeshi-taka-sign"></i>${tree.price}
     </p>
   </div>
-  <button
-    class="bg-[#15803D] text-[#fff] text-2xl px-5 py-3 rounded-full font-semibold w-full"
-  >
-    Donate Now
-  </button>
+     <button
+      class="bg-[#15803D] text-[#fff] text-2xl px-5 py-3 rounded-full font-semibold w-full add-cart"
+    >
+      Add to Cart
+    </button>
 
     `;
+
     treeContainer.append(treeDiv);
   }
 };
@@ -119,5 +147,3 @@ const DisplayCategories = (categories) => {
 };
 // Complete The category Data Function
 categoriesDataFunction();
-
-// class="rounded-t-md w-full object-cover h-full"
